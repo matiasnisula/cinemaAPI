0d3247f6376049c7e2eb2b5fef038ddb2bcb7550
Matias Nisula <matiasnisula81@gmail.com>
2023-05-02 11:58:32 +0300

    database context use postgre as database, create table Cinemas if doesnt exists

--- Program.cs
+++ Program.cs
@@ -1,11 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using cinemaAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+string connectionString = "Server=localhost;Port=5432;Database=database;User Id=admin;Password=admin;";
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<CinemaContext>(opt =>
+    opt.UseNpgsql(connectionString));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// TODO: Check whether the schema needs to be initialized
+
+var options = new DbContextOptionsBuilder<CinemaContext>()
+        .UseNpgsql(connectionString)
+        .Options;
+    using (var context = new CinemaContext(options))
+    {
+        context.Database.EnsureCreated();
+    }
 
 var app = builder.Build();
 
